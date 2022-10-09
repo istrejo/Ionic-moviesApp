0e6f1f09ac88a44f033462d5777ed55fc2fd2f15
istrejo <istrejo2106@gmail.com>
2022-10-09 17:13:11 -0400

    Buscador de películas (maquetación)

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonSearchbar } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  @ViewChild(IonSearchbar) serachBar: IonSearchbar;
+  findText: string = '';
+  ideas: string[] = [
+    'Spide-Man',
+    'La Vida es Bella',
+    'Thor Love and Thunder',
+    'The Batman',
+  ];
+  searching: boolean = false;
 
   constructor() {}
 
+  search() {
+    this.searching = true;
+    this.findText = this.serachBar.value;
+    console.log(this.serachBar);
+    console.log(this.findText);
+  }
+
+  // selectIdea(idea) {
+  //   this.serachBar.value = idea;
+  // }
 }
